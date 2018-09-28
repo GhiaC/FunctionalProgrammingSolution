2da7e1829c8c94423aad4b61d79bcbd4db137feb
masoud <ghiasy@aut.ac.ir>
2018-09-29 02:41:48 +0330

    changed vars name

--- src/main/scala/chapter3.scala
+++ src/main/scala/chapter3.scala
@@ -2,22 +2,22 @@
 
 sealed trait List[+A]
 
-case object Nill extends List[Nothing]
+case object Nil extends List[Nothing]
 
 case class cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
 
   def apply[A](as: A*): List[A] = {
-    if (as.isEmpty) Nill
+    if (as.isEmpty) Nil
     else cons(as.head, apply(as.tail: _*))
   }
 
   //exercise 2
   def tail[A](list: List[A]): List[A] = {
     //    list match {
-    case cons(head, tail) => tail
-    case Nill => Nill
+    case cons(_, tail) => tail
+    case Nil => Nil
     //    }
   }
 
@@ -30,7 +30,7 @@ object List {
   def drop[A](l: List[A], n: Int): List[A] = {
     (l, n) match {
       case (list, 0) => list
-      case (cons(_, xs), num) if num > 0 => drop(xs, num - 1)
+      case (cons(_, x), num) if num > 0 => drop(x, num - 1)
     }
   }
 
@@ -50,7 +50,7 @@ object List {
   def holdWhile[A](l: List[A], f: A => Boolean): List[A] = {
     l match {
       case cons(head, tail) if f(head) => cons(head, dropWhile(tail, f))
-      case cons(head, _) if !f(head) => cons(head, Nill)
+      case cons(head, _) if !f(head) => cons(head, Nil)
     }
   }
 
@@ -64,14 +64,14 @@ object List {
   //surprisingly function
   def append[A](list1: List[A], list2: List[A]): List[A] = {
     list1 match {
-      case Nill => list2
+      case Nil => list2
       case cons(head, tail) => cons(head, append(tail, list2))
     }
   }
 
   //exercise 6
   def init[A](l: List[A]): List[A] = {
-    case cons(head, cons(_, Nill)) => head
+    case cons(head, cons(_, Nil)) => head
     case cons(head, tail) => cons(head, init(tail))
   }
 
